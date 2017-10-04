0db5b6ad8af264aecbb0afa549eff157cf942b2d
WildemarElias <wildemar-elias@github.com>
2017-10-03 21:28:13 -0300

    v001
    Primeiro Exemplo

---
+++ WatchList/Models/Movie.cs
@@ -0,0 +1,13 @@
+namespace WatchList.Models{
+    public class Movie{
+        public string Title{get;set;}
+        public string Watched{get;set;}
+        public string Image{get;set;}
+        public string Grade{get;set;}
+
+
+    }
+
+
+
+}
---
+++ WatchList/Models/User.cs
@@ -0,0 +1,10 @@
+namespace WatchList.Models
+{
+    public class User
+    {
+
+        public string Name { get;set;}
+
+        public string Email{get;set;}
+    }
+}
